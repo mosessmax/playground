749e19d281b9b4162dd336497a989ed9e3d07dcb
adebayo-moses <adebayomoses597@gmail.com>
2024-08-25 00:31:30 +0100

    chore: react router add, tailwind setup, tiny fixes

--- src/components/RevealLink.tsx
+++ src/components/RevealLink.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { motion } from "framer-motion"
 
 const RevealLink = () => {
     return (
         <a href="/app">RevealLink</a>
     );
     }
+
+    export default RevealLink
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import RevealLink from './components/RevealLink.tsx'
 import './index.css'
-import {createBrowserRouter} from 'react-router-dom'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 
 const router = createBrowserRouter([
   // basename: '/app',
   // children: <App />
   { path : '/app', element: <App /> },
-  { path : '/contact', element: <Contact /> },
   { path : '/reveal-link', element: <RevealLink /> }
 ])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} />/
   </StrictMode>,
 )
